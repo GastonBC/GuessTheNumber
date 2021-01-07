66401450f48c002621568ae18c4dcdac9e24c54f
Gaston B <gastonbalparda@gmail.com>
2021-01-07 01:50:53 -0300

    Goods, checks, etc

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,11 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
+
+/*
+ TODO: Make two consoles, one for goods and regulars, other for help messages
+        Make regulars check, with str.Count(f => (f == ch)), this can work for goods as well
+        Make a drawing pane?
+ */
 
 namespace GuessTheNumber
 {
@@ -15,59 +18,11 @@ namespace GuessTheNumber
         string NPC_NUM;
         string PLAYER_NUM;
 
-
         public MainWindow()
         {
             InitializeComponent();
         }
 
-
-
-        private void Start_Click(object sender, RoutedEventArgs e)
-        {
-            // A new background thread is needed to process the files. The window thread remains available for the updates
-            Thread t = new Thread(() =>
-            {
-
-                try
-                {
-                  
-                }
-
-
-
-                catch (Exception ex)
-                {
-                    WriteLn("--------------------------");
-                    WriteLn(ex.GetType().ToString());
-                    WriteLn(ex.Message);
-                    WriteLn("");
-                    WriteLn(ex.StackTrace);
-                    WriteLn("--------------------------");
-
-
-                    b_Guess.Dispatcher.BeginInvoke(new Action(() =>
-                    { b_Guess.IsEnabled = true; }));
-                }
-
-            });
-
-            t.Start();
-        }
-
-
-
-        private void WriteLn(string text) // Invokes the console.text from the new thread and appends the line
-        {
-
-            ConsoleLog.Dispatcher.BeginInvoke(new Action(() =>
-            { ConsoleLog.Text += text + Environment.NewLine; }));
-        }
-
-        private void onTextChange(object sender, TextChangedEventArgs e)
-        {
-
-        }
 
         private void onPlayerTextChange(object sender, TextChangedEventArgs e)
         {
@@ -97,28 +52,99 @@ namespace GuessTheNumber
 
         private void Guess_Click(object sender, RoutedEventArgs e)
         {
-            WriteLn(tb_NumberTry.Text);
+            string GUESS_NUM = tb_NumberTry.Text;
+
+            if(!RunNumberChecks(GUESS_NUM, true)) return;
+
+            int GoodAmmount = 0;
+            int RegularAmmount = 0;
+
+
+            // First check good numbers
+            for(int i = 0; i < GUESS_NUM.Length; i++)
+            {
+               if (NPC_NUM[i] == GUESS_NUM[i])
+                {
+                    GoodAmmount++;
+                }
+            }
+
+            WriteLn(tb_NumberTry.Text + " - " + $"{GoodAmmount}G - {RegularAmmount}R");
         }
 
         private void ConfirmNum_Click(object sender, RoutedEventArgs e)
         {
-            // First check if first digit is 0
-            if (tb_PlayerNumber.Text.First().ToString() == "0")
+            PLAYER_NUM = tb_PlayerNumber.Text;
+
+            // if checks fail
+            if (!RunNumberChecks(PLAYER_NUM, true))
             {
-                WriteLn("Your number cannot begin with a 0");
                 return;
             }
-
-            PLAYER_NUM = tb_PlayerNumber.Text;
 
 
             int min = Convert.ToInt32(1 * (Math.Pow(10d, PLAYER_NUM.Length - 1)));    // 1* (10^x-1) - 1.  eg: 1*(10^4) = 10.000
             int max = Convert.ToInt32( (1 * (Math.Pow(10d, PLAYER_NUM.Length))) - 1); // 1* (10^x) - 1.  eg: 1*(10^5)-1 = 99.999
 
             Random rd = new Random();
-            NPC_NUM = rd.Next(min, max).ToString();
+
+            bool isNumberWrong = true;
+
+            while (isNumberWrong)
+            {
+                NPC_NUM = rd.Next(min, max).ToString();
+                if (RunNumberChecks(NPC_NUM, false))
+                {
+                    isNumberWrong = false;
+                }
+            }
 
             tb_NPCNumber.Text = NPC_NUM;
+
+            // b_ConfirmPlayer.IsEnabled = false;
+        }
+
+
+
+        private void WriteLn(string text) // Invokes the console.text from the new thread and appends the line
+        {
+
+            ConsoleLog.Dispatcher.BeginInvoke(new Action(() =>
+            { ConsoleLog.Text += text + Environment.NewLine; }));
+        }
+
+        private bool RunNumberChecks(string NUMBER, bool messages)
+        {
+            // First check if first digit is 0
+            if (NUMBER.First().ToString() == "0")
+            {
+                if (messages) WriteLn("Your number cannot begin with a 0");
+                return false;
+            }
+
+            // And if all chars are numbers
+            for (int i = 0; i < NUMBER.Length; i++)
+            {
+                if (!Char.IsDigit(NUMBER[i]))
+                {
+                    if (messages) WriteLn("All characters must be digits");
+                    return false;
+                }
+            }
+
+            // And if a number is repeated
+            foreach (char ch in NUMBER)
+            {
+                int freq = NUMBER.Count(f => (f == ch));
+
+                if (freq > 1)
+                {
+                    if (messages) WriteLn("You can only use a digit once");
+                    return false;
+                }
+            }
+
+            return true;
         }
     }
 }
