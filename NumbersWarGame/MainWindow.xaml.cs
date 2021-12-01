e25b5ccfa20856fb380fc8089745623ed4eb453f
GastonWkLaptop <gastonbalparda@gmail.com>
2021-12-01 17:00:35 -0300

    More cleaning

--- NumbersWarGame/MainWindow.xaml.cs
+++ NumbersWarGame/MainWindow.xaml.cs
@@ -176,31 +176,33 @@ namespace NumbersWarGame
         {
             // Now using this button to make the average win rate
 
-            int GamesToPlay = 100000;
+            int GamesToPlay = 10000;
             int Digits = 4;
             double TotalSteps = 0;
 
             for (int i = 0; i <= GamesToPlay; i++)
             {
-
-                // TODO: Make GetValidNumber more efficient
                 string NPC1Code = Utils.GetValidNumber(Utils.GetAllCodes(Digits));
 
                 Enemy NPC2 = new Enemy(Digits);
+
+                NPC2.MakeGuess();
                 int Steps = 1;
 
-                int Good = 0;
-                int Regular = 0;
-                string Guess = NPC2.MakeGuess();
-                Utils.AnswerToNumber(Guess, NPC1Code, out Good, out Regular);
+                int Good;
+                int Regular;
 
-                NPC2.Think(Guess, Good, Regular);
+                Utils.AnswerToNumber(NPC2.LastGuess, NPC1Code, out Good, out Regular);
 
-                while (Good != 4)
+                NPC2.Think(NPC2.LastGuess, Good, Regular);
+
+                while (NPC1Code != NPC2.LastGuess)
                 {
-                    Guess = NPC2.MakeGuess();
-                    Utils.AnswerToNumber(Guess, NPC1Code, out Good, out Regular);
-                    NPC2.Think(Guess, Good, Regular);
+                    NPC2.LastGuess = NPC2.MakeGuess();
+
+                    Utils.AnswerToNumber(NPC2.LastGuess, NPC1Code, out Good, out Regular);
+
+                    NPC2.Think(NPC2.LastGuess, Good, Regular);
 
                     Steps++;
                 }
